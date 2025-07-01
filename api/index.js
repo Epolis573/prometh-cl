cd46110b162cb682787fc5fea46fb383a7b1b8f0
Hai Anh <anhnh@phenikaa-x.com>
2025-07-01 11:13:40 +0700

    cdn

--- api/index.js
+++ api/index.js
@@ -3,6 +3,10 @@ const path = require("path");
 const app = express();
 const port = 8080;
 
+const BLOB_BASE_URL =
+  process.env.BLOB_BASE_URL ||
+  "https://mrfxxc2vy3ktalj1.public.blob.vercel-storage.com";
+
 // Serve static files locally, but NOT on Vercel
 if (process.env.VERCEL !== "1") {
   app.use(express.static(path.join(__dirname, "../public")));
@@ -14,13 +18,13 @@ app.set("view engine", "ejs");
 
 // Routes
 app.get("", (req, res) => {
-  res.render("index");
+  res.render("index", { BLOB_BASE_URL });
 });
 app.get("/index", (req, res) => {
-  res.render("index");
+  res.render("index", { BLOB_BASE_URL });
 });
 app.get("/news", (req, res) => {
-  res.render("news");
+  res.render("news", { BLOB_BASE_URL });
 });
 
 // Listen (only if not running as a Vercel serverless function)
